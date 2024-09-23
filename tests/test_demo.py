3545f03f1b6c25bcd8a6241a720028b9ff51e566
jamesgregson <james.gregson@gmail.com>
2024-09-22 19:31:58 -0700

    tweak

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,22 +1,24 @@
 import os
+import shutil
 import tempfile
 
 import illiterally
 import illiterally.cli
 
-def run_in_temp_directory():
+def run_in_temp_directory( test_dir ):
     def decorator( fn: callable ):
         def wrapper( *args, **kwargs ):
             cwd = os.getcwd()
-            with tempfile.TemporaryDirectory() as test_dir:
-                print(f'running test in {test_dir}')
-                os.chdir( test_dir )
-                fn( *args, test_dir=test_dir, **kwargs )
+            os.makedirs( test_dir )
+            print(f'running test in {test_dir}')
+            os.chdir( test_dir )
+            fn( *args, test_dir=test_dir, **kwargs )
             os.chdir(cwd)
+            shutil.rmtree( test_dir )
         return wrapper
     return decorator    
 
-@run_in_temp_directory()
+@run_in_temp_directory('tests/xhskjfdke')
 def test_demo( test_dir: str=None ):
     illiterally.cli.illiterally_demo()
     illiterally.cli.illiterally_cli( open('run.sh').read().split() )
